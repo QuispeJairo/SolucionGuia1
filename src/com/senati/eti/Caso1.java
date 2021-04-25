e2719b94dbfbdb8ff1a96ce7708bcabea8daa61d
GAMER <GAMER@DESKTOP-THK99AM>
2021-04-25 16:15:35 -0500

    Proyecto parcial - Guía1 (22/04)

---
+++ src/com/senati/eti/Caso1.java
@@ -0,0 +1,13 @@
+package com.senati.eti;
+
+public class Caso1 {
+
+	public static void main(String[] args) {
+		// Impresión de un mensaje
+		// CTRL + Barra de Escpacio
+		// CTRL + s -> Guardar los cambios
+		System.out.println("Bienvenido(a) Programador(a) Senatino(a)");
+	}
+
+}
+
